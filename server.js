8d293d6b96c883de48fa0f6ebc561edb28208b1f
Jesus Cepeda <jcepedaf@gmail.com>
2021-10-12 18:38:26 +0200

    agrego inClass (1) Get Exercise

--- server.js
+++ server.js
@@ -11,4 +11,11 @@ app.get("/quotes", function(request, response){
   response.json(quotes);
 });
 
+app.get("/quotes/:id", function(request, response){
+  const id= request.params.id;
+const quote = quotes.find(quote => quote.id == id);
+return response.send(quote)
+
+});
+
 app.listen(3000, () => console.log("Listening on port 3000"));
