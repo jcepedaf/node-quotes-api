de07533995dfeb88318e3c35a2633231c794bf25
Jesus Cepeda <jcepedaf@gmail.com>
2021-10-12 19:05:09 +0200

    agrego inClass (2) Post Exercise

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const quotes = require("./quotes.json");
 
 app.get('/', function(request, response) {
@@ -16,6 +18,16 @@ app.get("/quotes/:id", function(request, response){
 const quote = quotes.find(quote => quote.id == id);
 return response.send(quote)
 
+});
+
+app.post("/quotes", function(request, response){
+const quote = {
+  author: request.body.author,
+  quote: request.body.quote,
+  id: quotes.length
+};
+quotes.push(quote);
+return response.send(quote)
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
